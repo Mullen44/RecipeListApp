73b5b929ab22d67d68ce707b8a6b6e8aadcdc0fb
Mullen44 <65976580+Mullen44@users.noreply.github.com>
2021-11-29 16:10:36 -0800

    - Create/build UI in the featured view

--- recipeListApp_SwiftUI/Views/RecipeFeaturedView.swift
+++ recipeListApp_SwiftUI/Views/RecipeFeaturedView.swift
@@ -14,12 +14,67 @@ struct RecipeFeaturedView: View {
     @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        // Wrap tab view in Geometry Reader
+        VStack(alignment: .leading, spacing: 0){
+            Text("Featured Recipes")
+                .bold()
+                .padding(.leading)
+                .padding(.top, 40)
+                .font(.largeTitle)
+            
+            GeometryReader { geo in
+                    
+                
+                // Can also be used for swipeable views
+                TabView {
+                    
+                    // Loop through each recipe
+                    ForEach(0..<model.recipes.count) { index in
+                        
+                        if model.recipes[index].featured {
+                            
+                            // Create recipe card for each one that is featured
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                            
+                                VStack(spacing:0) {
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    Text(model.recipes[index].name)
+                                        .padding(5)
+
+                                }
+                            }
+                            .frame(width: geo.size.width-40, height: geo.size.height-100, alignment: .center)
+                            .cornerRadius(15)
+                            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y:5)
+                        }// End if statement
+                    }// End ForEach
+                }// End TabView
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                
+            }// End Geometry Reader
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("1 hour")
+                Text("Highlights")
+                    .font(.headline)
+                Text("Healthy, Hearty")
+            }
+            .padding([.leading, .bottom])
+        }
+    }// End Body
+}// End RecipeFeaturedView()
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
